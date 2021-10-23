68e31795010d3e0e61e3b66acfcd452837dcc10f
DustCar <dustcarmelo@yahoo.com>
2021-10-23 00:54:51 -0400

    Revert "added projectile spawner"
    This reverts commit 1ee436264f5cdb1e454963500ca6a9c1fe2e44f2.

--- GDC Game Jam/Assets/Dustin Scripts/Projectile.cs
+++ GDC Game Jam/Assets/Dustin Scripts/Projectile.cs
@@ -2,16 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileBehavior : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
     [SerializeField] private float velocity = 1f;
     [SerializeField] Rigidbody2D rb;
+    private int spawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        spawn = Random.Range(1, 4);
+        if (spawn == 1)
+        {
+            transform.position = new Vector2(10f, 4f);
+        }
+        else if (spawn == 2)
+        {
+            transform.position = new Vector2(10f, 0f);
+        }
+        else
+        {
+            transform.position = new Vector2(10f, -4f);
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -20,9 +34,10 @@ public class ProjectileBehavior : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "city")
+        if (collision.gameObject.tag =="city")
         {
             Destroy(gameObject);
         }
     }
+
 }
