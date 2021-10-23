1ee436264f5cdb1e454963500ca6a9c1fe2e44f2
DustCar <dustcarmelo@yahoo.com>
2021-10-22 22:52:19 -0400

    added projectile spawner

--- GDC Game Jam/Assets/Dustin Scripts/ProjectileBehavior.cs
+++ GDC Game Jam/Assets/Dustin Scripts/ProjectileBehavior.cs
@@ -2,30 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class ProjectileBehavior : MonoBehaviour
 {
     [SerializeField] private float velocity = 1f;
     [SerializeField] Rigidbody2D rb;
-    private int spawn;
-
     // Start is called before the first frame update
     void Start()
     {
-        spawn = Random.Range(1, 4);
-        if (spawn == 1)
-        {
-            transform.position = new Vector2(10f, 4f);
-        }
-        else if (spawn == 2)
-        {
-            transform.position = new Vector2(10f, 0f);
-        }
-        else
-        {
-            transform.position = new Vector2(10f, -4f);
-        }
-
+        
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -34,10 +20,9 @@ public class Projectile : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag =="city")
+        if (collision.gameObject.tag == "city")
         {
             Destroy(gameObject);
         }
     }
-
 }
